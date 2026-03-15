e57b711763cd073a44e0e1a193c1e069d72987d1
PakYouMu <21-1-00398@vsu.edu.ph>
2026-03-16 06:14:52 +0800

    repo: routing fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+    base: './', // Use relative paths for built assets
     plugins: [
         vue(),
         tailwindcss()
